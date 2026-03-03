b09910ecd13af9b3183d76b88e2b0f0a1f5a594c
QBeLock <109404073+QBeLock@users.noreply.github.com>
2026-03-03 12:00:52 +0100

    ach jaj

--- Player sheet maker.py
+++ Player sheet maker.py
@@ -1,5 +1,7 @@
 import json
 import tkinter as tk
+from abc import ABC, abstractmethod
+from asyncio.windows_events import NULL
 from tkinter import filedialog
 
 root = tk.Tk()
@@ -8,51 +10,80 @@ root.title("Player sheet maker")
 
 
 
-class zakladni_riadok():
-    data = {}
-    riadok = -1
-    def __init__(self, nazov, grid_velkost = riadok):
-        zakladni_riadok.riadok += 1
-        self.nazov = tk.Label(root, text=nazov)
-        self.nazov.grid(row=zakladni_riadok.riadok, column=0, pady=(0,grid_velkost))
-        zakladni_riadok.data.update({nazov: ""})
+class zakladni_riadok(ABC):
+    riadky = []
+    def __init__(self, nazov, grid_velkost = 0):
+        zakladni_riadok.riadky.append(self)
+        self.nazov = nazov
+        self.lbl = tk.Label(root, text=nazov)
+        self.lbl.grid(row=len(zakladni_riadok.riadky) - 1, column=0, pady=(0,grid_velkost))
+        self._grid_velkost = grid_velkost
+
+    def _init_end(self):
+        self.okno.grid(row=len(zakladni_riadok.riadky) - 1, column=1, pady=(0,self._grid_velkost))
+
+    @abstractmethod
+    def get(self):
+        pass
+
+    def set(self, value):
+        self._data = value
 
 class textovi_vstup(zakladni_riadok):
-    text = tk.StringVar()
+
 
     def __init__(self, nazov, grid_velkost = 0):
+        self._data = tk.StringVar()
         super().__init__(nazov,grid_velkost)
-        self.okno = tk.Entry(root, textvariable=zakladni_riadok.data[nazov], width=50)
-        self.okno.grid(row=zakladni_riadok.riadok, column=1, pady=(0,grid_velkost))
+        self.okno = tk.Entry(root, textvariable=self._data, width=50)
+        self._init_end()
 
     def get(self):
-        return self.text.get()
-
-class session_choice_bool(zakladni_riadok):
-    def __init__(self, nazov,grid_velkost = 0):
-        super().__init__(nazov,grid_velkost)
-        zakladni_riadok.data[nazov] = []
-        self.session_input = []
-        self.frame = tk.Frame(root)
-        for i in range(3):
-            zakladni_riadok.data[nazov].append(tk.BooleanVar())
-            self.session_input.append(tk.Checkbutton(self.frame, variable=zakladni_riadok.data[nazov][i]))
-            self.session_input[i].grid(row=0, column=i + 1, padx=20)
-        self.frame.grid(row=zakladni_riadok.riadok, column=1, pady=(0,grid_velkost))
-
-class session_choice_int(zakladni_riadok):
+        return self._data.get()
 
 
-    def __init__(self, nazov, grid_velkost = 0):
+
+class session_choice_base(zakladni_riadok, ABC):
+
+    @abstractmethod
+    def create_session(self, session_id):
+        pass
+
+    def __init__(self, nazov, grid_velkost=0):
         super().__init__(nazov, grid_velkost)
-        zakladni_riadok.data[nazov] = []
+        self._data = []
+        self.session = []
         self.session_input = []
-        self.frame = tk.Frame(root)
+        self.okno = tk.Frame(root)
         for i in range(3):
-            zakladni_riadok.data[nazov].append(tk.IntVar())
-            self.session_input.append(tk.Entry(self.frame, textvariable=zakladni_riadok.data[nazov][i], width=1))
+            self._data.append(NULL)
+            self.session_input.append(self.create_session(i))
             self.session_input[i].grid(row=0, column=i + 1, padx=30, pady=0)
-        self.frame.grid(row=zakladni_riadok.riadok, column=1, pady=(0,grid_velkost))
+        self._init_end()
+
+    def get(self):
+        konvertovane = []
+        for i in self.session:
+            konvertovane.append(i.get())
+        return konvertovane
+
+
+class session_choice_bool(session_choice_base):
+    def create_session(self, session_id):
+        self.session.append(tk.BooleanVar())
+        return tk.Checkbutton(self.okno, variable=self.session[session_id])
+
+
+
+class session_choice_int(session_choice_base):
+    def create_session(self, session_id):
+        self.session.append(tk.IntVar())
+        return tk.Entry(self.okno, textvariable=self.session[session_id], width=1)
+
+
+
+
+
 
 
 meno = textovi_vstup("Meno", 10)
@@ -67,21 +98,41 @@ session_choice_int("Minor eventi")
 session_choice_bool("Major eventi")
 
 
+pouzivani_peiecinok = ""
+
+def konvertovat_data():
+    konvertovane = {}
+    for i in zakladni_riadok.riadky:
+        konvertovane.update({i.nazov: i.get()})
+    return konvertovane
+
 def save():
-    file_path = filedialog.askopenfilename(title="Select Profile Picture", filetypes=[("JSON", ('*.json'))], defaultextension=".json")
+    if pouzivani_peiecinok == "":
+        save_as()
+    else:
+        with open(pouzivani_peiecinok, "w") as json_file:
+            json_file.write(json.dumps(konvertovat_data()))
+            json_file.close()
 
-
+def save_as():
+    file_path = filedialog.asksaveasfilename(title="Save as", filetypes=[("JSON", ('*.json'))], defaultextension=".json")
+    pouzivani_peiecinok = file_path
+    save()
 
 def load():
-    file_path = filedialog.askopenfilename(title="Select Profile Picture", filetypes=[("JSON", ('*.json'))])
+    file_path = filedialog.askopenfilename(title="Load", filetypes=[("JSON", ('*.json')),("All files", "*.*")], defaultextension=".json")
     with open(file_path) as json_file:
-        data = json.load(json_file)
+        json_dir = json.load(json_file)
+        for i in zakladni_riadok.riadky:
+            i.set(json_dir[i.nazov])
+        json_file.close()
 
-
-load = tk.Button(root, text="Load", command=load)
-load.grid(row=6, column=2)
-save = tk.Button(root, text="Save", command=save)
-save.grid(row=7, column=2)
+load_btn = tk.Button(root, text="Load", command=load)
+load_btn.grid(row=5, column=2)
+save_btn = tk.Button(root, text="Save", command=save)
+save_btn.grid(row=6, column=2)
+save_as_btn = tk.Button(root, text="Save as", command=save_as)
+save_as_btn.grid(row=7, column=2)
 
 
 root.mainloop()
