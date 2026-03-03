3d23ec7746e62fda95892722a8d28a86aa409049
QBeLock <109404073+QBeLock@users.noreply.github.com>
2026-03-03 15:01:50 +0100

    tooltip

--- Player sheet maker.py
+++ Player sheet maker.py
@@ -8,15 +8,56 @@ root = tk.Tk()
 root.title("Player sheet maker")
 
 
+class ToolTip(object):
 
+    def __init__(self, widget):
+        self.widget = widget
+        self.tipwindow = None
+        self.id = None
+        self.x = self.y = 0
+
+    def showtip(self, text):
+        "Display text in tooltip window"
+        self.text = text
+        if self.tipwindow or not self.text:
+            return
+        x, y, cx, cy = self.widget.bbox("insert")
+        x = x + self.widget.winfo_rootx() + 57
+        y = y + cy + self.widget.winfo_rooty() +27
+        self.tipwindow = tw = tk.Toplevel(self.widget)
+        tw.wm_overrideredirect(1)
+        tw.wm_geometry("+%d+%d" % (x, y))
+        label = tk.Label(tw, text=self.text,
+                      background="#ffffe0", borderwidth=1,
+                      font=("tahoma", "8", "normal"))
+        label.pack(ipadx=1)
+
+    def hidetip(self):
+        tw = self.tipwindow
+        self.tipwindow = None
+        if tw:
+            tw.destroy()
+
+def CreateToolTip(widget, text):
+    toolTip = ToolTip(widget)
+    def enter(event):
+        toolTip.showtip(text)
+    def leave(event):
+        toolTip.hidetip()
+    widget.bind('<Enter>', enter)
+    widget.bind('<Leave>', leave)
 
 class zakladni_riadok(ABC):
     riadky = []
-    def __init__(self, nazov, grid_velkost = 0):
+    skupina = root
+    def __init__(self, nazov, tooltip, grid_velkost = 0, nova_skupina=False):
+        if nova_skupina:
+            zakladni_riadok.skupina = tk.Frame(root, bg="gray")
         zakladni_riadok.riadky.append(self)
         self.nazov = nazov
-        self.lbl = tk.Label(root, text=nazov)
+        self.lbl = tk.Label(zakladni_riadok.skupina, text=nazov)
         self.lbl.grid(row=len(zakladni_riadok.riadky) - 1, column=0, pady=(0,grid_velkost))
+        CreateToolTip(self.lbl, tooltip)
         self._grid_velkost = grid_velkost
 
     def _init_end(self):
@@ -33,10 +74,10 @@ class zakladni_riadok(ABC):
 class textovi_vstup(zakladni_riadok):
 
 
-    def __init__(self, nazov, grid_velkost = 0):
+    def __init__(self, nazov, tooltip, grid_velkost = 0, nova_skupina=False):
+        super().__init__(nazov,tooltip,grid_velkost,nova_skupina)
         self._data = tk.StringVar()
-        super().__init__(nazov,grid_velkost)
-        self.okno = tk.Entry(root, textvariable=self._data, width=50)
+        self.okno = tk.Entry(zakladni_riadok.skupina, textvariable=self._data, width=50)
         self._init_end()
 
     def get(self):
@@ -53,12 +94,12 @@ class session_choice_base(zakladni_riadok, ABC):
     def create_session(self, session_id):
         pass
 
-    def __init__(self, nazov, grid_velkost=0):
-        super().__init__(nazov, grid_velkost)
+    def __init__(self, nazov, tooltip, grid_velkost=0, nova_skupina=False):
+        super().__init__(nazov,tooltip,grid_velkost,nova_skupina)
         self._data = []
         self.session = []
         self.session_input = []
-        self.okno = tk.Frame(root)
+        self.okno = tk.Frame(zakladni_riadok.skupina)
         for i in range(3):
             self._data.append(NULL)
             self.session_input.append(self.create_session(i))
@@ -94,16 +135,16 @@ class session_choice_int(session_choice_base):
 
 
 
-meno = textovi_vstup("Meno", 10)
+meno = textovi_vstup("Meno","Tvoje meno debil", 10)
 
-meno_postavi = textovi_vstup("Meno Postavi")
-podstatne_pre_postavu = textovi_vstup("Podstatne Pre-Postavi",10)
+meno_postavi = textovi_vstup("Meno Postavi", "daj dáke cool meno nemusíš nam ho potom ani povedať podla roleplayu",nova_skupina=True)
+podstatne_pre_postavu = textovi_vstup("Podstatne Pre Postavi","Načom tvojej postave záleží. niečo čo ked sa stane tak na to bude reagovať.",10)
 
-plot_twist = textovi_vstup("Plot Twist")
-session_choice_bool("Plot twist")
-dalsie_plani = textovi_vstup("Dalšie Pláni")
-session_choice_int("Minor eventi")
-session_choice_bool("Major eventi")
+plot_twist = textovi_vstup("Plot twist", "niečo vimislíš")
+session_choice_bool("Plot twist session", "kedi sa stane/ú tvoj(e) plot twisti. Každé okienko je session z lava do prava.")
+dalsie_plani = textovi_vstup("Ďalšie Pláni", "Rôzne eventi, situacie alebo miesta ktore sa mozu obiavit v kampani")
+session_choice_int("Minor eventi", "kedi sa stanú predom pripravené veci ktoré si vimislel ale len na velmi kráatko napr: najdeme zbran.\n Každé okienko je session z lava do prava napíš tam kolko sa ich má stať za daní session.")
+session_choice_bool("Major eventi", "Rovnako ako minor eventi ale tieto môžu zabrať aj pol sessionu")
 
 
 
@@ -117,6 +158,7 @@ def konvertovat_data():
 file_path = ""
 
 def save():
+    print(file_path + "---")
     if file_path == "":
         save_as()
     else:
@@ -126,15 +168,13 @@ def save():
 
 def save_as():
     try:
-        file_path = filedialog.asksaveasfilename(title="Save as", filetypes=[("JSON", ('*.json'))], defaultextension=".json")
+        file_path = filedialog.asksaveasfilename(title="Save as", filetypes=[("JSON", ('*.json'))], defaultextension=".json", initialfile=zakladni_riadok.riadky[0].get()+" sheet")
         with open(file_path, "w") as json_file:
             json_file.write(json.dumps(konvertovat_data()))
             json_file.close()
+            print(file_path)
     except FileNotFoundError:
-        pouzivani_subor = open(file_path, "w")
-    finally:
-        print(file_path)
-        zapisat_do_suboru()
+        pass
 
 def load():
     file_path = filedialog.askopenfilename(title="Load", filetypes=[("JSON", ('*.json')),("All files", "*.*")], defaultextension=".json")
@@ -142,8 +182,8 @@ def load():
         json_dir = json.loads(json_file.read())
         for i in zakladni_riadok.riadky:
             i.set(json_dir[i.nazov])
-            print(i.nazov)
         json_file.close()
+        print(file_path)
 
 load_btn = tk.Button(root, text="Load", command=load)
 load_btn.grid(row=5, column=2)
